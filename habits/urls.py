0c94965cd3f1d4e7c15bac20c5042fb79c50cc98
Alexandra <alexandrasolovjova@mail.ru>
2023-11-02 19:12:55 +0300

    fix habit urls names

--- habits/urls.py
+++ habits/urls.py
@@ -6,9 +6,9 @@ from habits.views import *
 appname = HabitsConfig.name
 
 url_patterns = [
-    path('/', HabitListView.as_view(), name='lesson_list'),
-    path('<int:pk>', HabitDetailView.as_view(), name='lesson_detail'),
-    path('create/', HabitCreateView.as_view(), name='lesson_create'),
-    path('edit/<int:pk>', HabitUpdateView.as_view(), name='lesson_edit'),
-    path('delete/<int:pk>', HabitDestroyView.as_view(), name='lesson_delete'),
+    path('/', HabitListView.as_view(), name='habit_list'),
+    path('<int:pk>', HabitDetailView.as_view(), name='habit_detail'),
+    path('create/', HabitCreateView.as_view(), name='habit_create'),
+    path('edit/<int:pk>', HabitUpdateView.as_view(), name='habit_edit'),
+    path('delete/<int:pk>', HabitDestroyView.as_view(), name='habit_delete'),
 ]
