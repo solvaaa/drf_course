bdc4c3ddd9c773e795f2723a300fc49ec7999316
Alexandra <alexandrasolovjova@mail.ru>
2023-11-02 19:25:30 +0300

    fix urls

--- habits/urls.py
+++ habits/urls.py
@@ -6,8 +6,8 @@ from habits.views import *
 appname = HabitsConfig.name
 
 urlpatterns = [
-    path('/', HabitListView.as_view(), name='habit_list'),
-    path('/<int:pk>', HabitDetailView.as_view(), name='habit_detail'),
+    path('', HabitListView.as_view(), name='habit_list'),
+    path('<int:pk>', HabitDetailView.as_view(), name='habit_detail'),
     path('create/', HabitCreateView.as_view(), name='habit_create'),
     path('edit/<int:pk>', HabitUpdateView.as_view(), name='habit_edit'),
     path('delete/<int:pk>', HabitDestroyView.as_view(), name='habit_delete'),
