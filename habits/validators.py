2e03d1b2d9e2b1c5f30b427c638f83f4b8cb5782
Alexandra <alexandrasolovjova@mail.ru>
2023-11-02 20:27:03 +0300

    modify Duration Validator

--- habits/validators.py
+++ habits/validators.py
@@ -2,11 +2,14 @@ from rest_framework.serializers import ValidationError
 
 
 class DurationValidator:
-
     def __init__(self, field):
         self.field = field
 
     def __call__(self, value):
         dict_value = dict(value).get(self.field)
         if dict_value > 120:
-            raise ValidationError('Слишком долгое время выполнения привычки')
+            raise ValidationError('Время выполнения привычки должно быть '
+                                  'не больше 120 секунд')
+        elif dict_value <= 0:
+            raise ValidationError('Неверное время выполнения привычки')
+
