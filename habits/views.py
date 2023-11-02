407bebe5cdf1b561eaa641fa1217bcb51d7705f8
Alexandra <alexandrasolovjova@mail.ru>
2023-11-02 20:13:23 +0300

    add ordering by id

--- habits/views.py
+++ habits/views.py
@@ -1,4 +1,6 @@
+from rest_framework.filters import OrderingFilter
 from rest_framework.generics import RetrieveAPIView, ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from habits.models import Habit
 from habits.serializers import HabitSerializer
@@ -6,7 +8,7 @@ from habits.serializers import HabitSerializer
 
 class HabitDetailView(RetrieveAPIView):
     """Shows details of the habit"""
-    queryset = Habit.objects.all()
+    queryset = Habit.objects.all().order_by('id')
     serializer_class = HabitSerializer
 
 
@@ -15,11 +17,15 @@ class HabitListView(ListAPIView):
     serializer_class = HabitSerializer
     queryset =Habit.objects.all()
 
+    filter_backends = [OrderingFilter]
+    ordering_fields =['id']
+
 
 class HabitCreateView(CreateAPIView):
     """Creates new habit"""
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         obj = serializer.save()
