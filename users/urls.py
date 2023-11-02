90dd99d6738741a1f2a4e2b7725d71a39ea18e76
Alexandra <alexandrasolovjova@mail.ru>
2023-11-02 19:09:54 +0300

    add users appname to urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from users.apps import UsersConfig
+
+appname = UsersConfig.name
+
+
 urlpatterns = [
     path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
