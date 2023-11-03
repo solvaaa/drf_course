11faed44083bd0686eba1bfd230c334c606c8ac6
Alexandra <alexandrasolovjova@mail.ru>
2023-11-03 14:28:45 +0300

    add register urls

--- config/urls.py
+++ config/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('token/', include(('users.urls', 'users'), namespace='users')),
+    path('user/', include(('users.urls', 'users'), namespace='users')),
     path('', include(("habits.urls", 'habits'), namespace='habits')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
