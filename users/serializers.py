b14473fd7c483398e3056f10e6d9c21756df12cb
Alexandra <alexandrasolovjova@mail.ru>
2023-11-03 14:44:42 +0300

    pep8

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from users.models import User
@@ -16,4 +15,10 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'first_name', 'last_name', 'phone')
+        fields = (
+            'id',
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'phone')
