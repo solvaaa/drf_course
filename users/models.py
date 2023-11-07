7bb1f230f7d12a90a2b40e5baea03618b60b1a2d
Alexandra <alexandrasolovjova@mail.ru>
2023-11-07 21:35:46 +0300

    made telegram_handle field unique

--- users/models.py
+++ users/models.py
@@ -8,7 +8,10 @@ NULLABLE = {'blank': True, 'null': True}
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
-    telegram_handle = models.CharField(max_length=100, verbose_name='Telegram', **NULLABLE)
+    telegram_handle = models.CharField(
+        max_length=100,
+        verbose_name='Telegram',
+        unique=True, **NULLABLE)
     chat_id = models.CharField(max_length=100, verbose_name='id чата в Телеграме', **NULLABLE)
     subscribed_to_bot = models.BooleanField(default=False, verbose_name='подписан на бота?')
 
